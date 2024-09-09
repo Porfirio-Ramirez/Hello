29fc55f36bb05319912937fba5f5036c84afd444
Porfirio-Ramirez <Porfirioramirez999@gmail.com>
2024-09-08 22:51:47 -0400

    add new

---
+++ Hello/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num=10;
+
+if (num<=10)
+{
+  Console.WriteLine("Hello, World!");
+} else
+{
+    Console.WriteLine("good bye");
+}
+
